22cfb14cd5d15a4dae1f364a39824d8069c1c054
Piyush <piyushshinde1902@gmail.com>
2024-01-06 22:51:17 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
-// const scheduledFunctions = require('./express/functions/cron');
 
 const app = require('./express/server');
-
-// ADD CALL to execute your function(s)
-// scheduledFunctions.initScheduledJobs();
 
 app.listen(4000, () => console.log('Local app listening on port 4000!'));
